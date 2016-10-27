18bfe3d950629f3490d83992cf21b2e780b50d47
Long (Lucas) TRAN <lucas@enclave.vn>
2016-10-27 11:21:39 +0700

    [] Modify centerOfScreenWidth to pin selected menu to left corner

--- Pod/Classes/MenuView.swift
+++ Pod/Classes/MenuView.swift
@@ -62,7 +62,8 @@ open class MenuView: UIScrollView {
         } else {
             screenWidth = UIScreen.main.bounds.width
         }
-        return menuItemViews[currentPage].frame.midX - screenWidth / 2
+//        return menuItemViews[currentPage].frame.midX - screenWidth / 2
+        return menuItemViews[currentPage].frame.origin.x - 10.0
     }
     fileprivate var contentOffsetXForCurrentPage: CGFloat {
         guard menuItemCount > MinimumSupportedViewCount else { return 0.0 }
